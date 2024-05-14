ca4077097e7061a83bb34b04fcc8b9f624b5ec4c
borsuk <b1rsu_k@protonmail.ch>
2024-05-14 09:47:18 -0400

    lb 10 linux

--- main.c
+++ main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
+void get_cpu_info() {
+    FILE *fp;
+    char buffer[1024];
+
+    fp = fopen("/proc/cpuinfo", "r");
+    if (fp == NULL) {
+        printf("Failed to open /proc/cpuinfo\n");
+        return;
+    }
+
+    printf("CPU інформація:\n");
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(fp);
+}
+
+void get_memory_info() {
+    FILE *fp;
+    char buffer[1024];
+
+    fp = fopen("/proc/meminfo", "r");
+    if (fp == NULL) {
+        printf("Failed to open /proc/meminfo\n");
+        return;
+    }
+
+    printf("\nІнформація про пам'ять:\n");
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(fp);
+}
+
+int main() {
+    get_cpu_info();
+    get_memory_info();
+
     return 0;
 }
